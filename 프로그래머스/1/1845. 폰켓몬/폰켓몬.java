d602788ff2b83bfcb1fc05d9a0177efe9e8320b2
beanteacher <99578261+beanteacher@users.noreply.github.com>
2024-08-14 10:42:11 +0900

    Update 폰켓몬.java

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -3,15 +3,14 @@ import java.util.Set;
 
 class Solution {
      public int solution(int[] nums) {
-        int answer = 0;
-        int count =  nums.length / 2;
+        int answer = nums.length / 2;
+         
         Set<Integer> set = new HashSet<>();
         for(int i = 0; i < nums.length; i++) {
             set.add(nums[i]);
         }
         
-        if(set.size() >= count) answer = count;
-        else answer = set.size();
+        if(set.size() < answer) answer = set.size();
          
         return answer;
     }
