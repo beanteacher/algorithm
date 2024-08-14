85011a055891e54f9203caab65512ba03d9b2bd4
beanteacher <99578261+beanteacher@users.noreply.github.com>
2024-08-14 10:44:43 +0900

    Update 이상한 문자 만들기.java

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -4,19 +4,17 @@ class Solution {
         int index = 0;
 
         for(int i = 0; i < s.length(); i++) {
-
-            if(Character.valueOf(s.charAt(i)).toString().equals(" ")) {
-                answer += " ";
-                index = 0;
-                continue;
-            }
-
-            if(index % 2 == 0) {
+            if(index % 2 == 0) {  // index가 짝수 -> 대문자로 변경
                 answer += Character.valueOf(s.charAt(i)).
                         toString().toUpperCase();
-            } else {
+            } else { // index가 홀수 -> 소문자로 변경
                 answer +=  Character.valueOf(s.charAt(i)).
                         toString().toLowerCase();
+            }
+            
+            if(Character.valueOf(s.charAt(i)).toString().equals(" ")) { // 띄워쓰기가 나오면 index 0으로 초기화
+                index = 0;
+                continue;
             }
             index++;
         }
