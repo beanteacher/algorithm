1b7fdbccbc6815ddf19916b5c7d85f2120220e62
beanteacher <99578261+beanteacher@users.noreply.github.com>
2024-08-14 10:43:33 +0900

    Update ［PCCE 기출문제］ 9번 ／ 이웃한 칸.java

--- 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.java
+++ 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.java
@@ -5,16 +5,13 @@ class Solution {
         int[] dh = {0, 1, -1, 0};
         int[] dw = {1, 0, 0, -1};
         
-        
-            for(int i = 0; i < dh.length; i++) {
-                try {
-                    if(check.equals(board[h-dh[i]][w-dw[i]])) answer++;
-                } catch(Exception e) {
-            
-                }          
-            }
-        
-        
+        for(int i = 0; i < dh.length; i++) {
+            try {
+                if(check.equals(board[h-dh[i]][w-dw[i]])) answer++;
+            } catch(Exception e) {
+                // IndexOutOfException 발생 시 배열이 존재하지 않기 때문에 answer 증가X
+            }          
+        }
         
         return answer;
     }
