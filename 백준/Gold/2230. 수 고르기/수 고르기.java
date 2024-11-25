a290754a4bdd527e4eee070f4503b98fe4dd81bd
beanteacher <99578261+beanteacher@users.noreply.github.com>
2024-11-25 23:52:12 +0900

    [Gold V] Title: 수 고르기, Time: 1568 ms, Memory: 30356 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.java
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken()); 
         int m = Integer.parseInt(st.nextToken());
 
         int[] nArr = new int[n];
@@ -14,7 +14,7 @@ public class Main {
         }
         Arrays.sort(nArr);
         
-        int result = Integer.MAX_VALUE;
+        int result = 2000000001; // result 초기화를 배열의 마지막 값 혹은 문제 조건(0 ≤ |A[i]| ≤ 1,000,000,000)에 의한 1,000,000,001으로 초기화 시 왜 작동하지 않는가 ?
 
         for(int i = 0; i < n; i++) {
             for(int j = i+1; j < n; j++) {
@@ -22,8 +22,9 @@ public class Main {
                 if(diff >= m) {
                     if(result > diff) {
                         result = diff;
-                    }
-                    break;
+                    } // result = Math.min(result, diff)로 수정 가능
+                    
+                    break; // result는 최소값을 구하는 것이니 이 이상 더 큰 차이는 필요 없으므로 break
                 }
             }
         }
