cf3a073a6a639fd3bc48d9c7dc82d048a2c3cc30
beanteacher <99578261+beanteacher@users.noreply.github.com>
2025-01-09 17:41:04 +0900

    [Silver III] Title: Java vs C++, Time: 112 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Silver/3613. Java vs C＋＋/Java vs C＋＋.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String word = br.readLine();
+        char[] words = word.toCharArray();
+        StringBuilder result = new StringBuilder();
+
+        boolean isError = false;
+
+        if(!Character.isLowerCase(word.charAt(0)) // 첫글자가 소문자가 아닌 경우 에러
+                || word.charAt(word.length() - 1) == '_' // 마지막 문자가 _인 경우 에러
+                || (word.contains("_") && word.chars().anyMatch(Character::isUpperCase))) { // _도 있고 대문자도 있는 경우 에러 
+            
+            System.out.println("Error!");
+            return;
+        }
+
+        for(int i = 0; i < words.length; i++) {
+            if(words[i] == '_') {
+                char nextWord = words[++i];
+
+                if(Character.isLowerCase(nextWord)) {
+                    result.append(Character.toUpperCase(nextWord));
+                } else {
+                    isError = true;
+                    break;
+                }
+            } else if(Character.isUpperCase(words[i])) {
+                result.append("_").append(Character.toLowerCase(words[i]));
+            } else if(Character.isLowerCase(words[i])){
+                result.append(words[i]);
+            } else {
+                isError = true;
+                break;
+            }
+        }
+
+        System.out.println(isError ? "Error!" : result);
+    }
+}
