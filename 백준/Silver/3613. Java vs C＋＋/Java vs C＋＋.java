fe777b0a13ca72f3bd4d9c69ec37def269511a29
beanteacher <99578261+beanteacher@users.noreply.github.com>
2025-01-09 17:41:42 +0900

    [Silver III] Title: Java vs C++, Time: 108 ms, Memory: 14376 KB -BaekjoonHub

--- 백준/Silver/3613. Java vs C＋＋/Java vs C＋＋.java
+++ 백준/Silver/3613. Java vs C＋＋/Java vs C＋＋.java
@@ -12,8 +12,8 @@ public class Main {
 
         if(!Character.isLowerCase(word.charAt(0)) // 첫글자가 소문자가 아닌 경우 에러
                 || word.charAt(word.length() - 1) == '_' // 마지막 문자가 _인 경우 에러
-                || (word.contains("_") && word.chars().anyMatch(Character::isUpperCase))) { // _도 있고 대문자도 있는 경우 에러 
-            
+                || (word.contains("_") && word.chars().anyMatch(Character::isUpperCase))) { // _도 있고 대문자도 있는 경우 에러
+
             System.out.println("Error!");
             return;
         }
@@ -22,11 +22,11 @@ public class Main {
             if(words[i] == '_') {
                 char nextWord = words[++i];
 
-                if(Character.isLowerCase(nextWord)) {
-                    result.append(Character.toUpperCase(nextWord));
-                } else {
+                if(nextWord == '_') {
                     isError = true;
                     break;
+                } else {
+                    result.append(Character.toUpperCase(nextWord));
                 }
             } else if(Character.isUpperCase(words[i])) {
                 result.append("_").append(Character.toLowerCase(words[i]));
