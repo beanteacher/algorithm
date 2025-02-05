0b6274d35d3d67ad149c2b1f8dd92329ed34648c
beanteacher <99578261+beanteacher@users.noreply.github.com>
2025-02-05 10:43:38 +0900

    [Silver I] Title: 센티와 마법의 뿅망치, Time: 320 ms, Memory: 28836 KB -BaekjoonHub

--- 백준/Silver/19638. 센티와 마법의 뿅망치/센티와 마법의 뿅망치.java
+++ 백준/Silver/19638. 센티와 마법의 뿅망치/센티와 마법의 뿅망치.java
@@ -12,30 +12,33 @@ public class Main {
         int t = Integer.parseInt(st.nextToken());
 
         PriorityQueue<Integer> heap = new PriorityQueue<>((a,b) -> b - a);
-        
         for(int i = 0; i < n; i++) {
             heap.offer(Integer.parseInt(br.readLine()));
         }
-        
 
-				int hitCount = 0;
         for(int i = 0; i < t; i++) {
-        
-            // 거인이 centi보다 작거나 1이면 끝내기
-            if((heap.peek() < centiHeight) || (heap.peek() == 1)) {
-		            break;
+            // centi보다 작아지면 끝내기
+            if(heap.peek() < centiHeight) {
+                System.out.println("YES");
+                System.out.println(i);
+                
+                return;
             }
-            
-            heap.offer(heap.poll() / 2);
-            hitCount++;
+            // MaxHeap에서 가장 큰 값이 1이 아니면 때린다.
+            if(heap.peek() == 1) {
+                t = i;
+                break;
+            } else {
+                heap.offer(heap.poll() / 2);
+            }
         }
 
         if(heap.peek() >= centiHeight) {
             System.out.println("NO");
-            System.out.println(heap.peek());
+            System.out.println(heap.poll());
         } else {
             System.out.println("YES");
-            System.out.println(hitCount);
+            System.out.println(t);
         }
     }
 }
