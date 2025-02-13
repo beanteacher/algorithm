041c1e2ca0256da4ed11866d536fe1443e8990b6
beanteacher <99578261+beanteacher@users.noreply.github.com>
2025-02-13 12:12:45 +0900

    [Gold III] Title: 두 배열의 합, Time: 484 ms, Memory: 95728 KB -BaekjoonHub

--- 백준/Gold/2143. 두 배열의 합/두 배열의 합.java
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.java
@@ -22,66 +22,37 @@ public class Main {
             mArr[i] = Integer.parseInt(st.nextToken());
         }
 
-        // n의 부분 합을 담아주기 위해 nList에 n의 부분 합을 저장한다.
-        List<Integer> nList = new ArrayList<>();
+        // n의 부분 합을 담아주기 위해 nMap에 n의 부분 합과 부분 합의 개수을 저장한다.
+        Map<Integer, Integer> nMap = new HashMap<>();
         for(int i = 0; i < n; i++) {
             int sum = 0;
             for(int j = i; j < n; j++) {
                 sum += nArr[j];
 
-                nList.add(sum);
+                nMap.put(sum, nMap.getOrDefault(sum, 0) + 1);
             }
         }
 
-        // m의 부분 합을 담아주기 위해 mList에 m의 부분 합을 저장한다.
-        List<Integer> mList = new ArrayList<>();
+        // m의 부분 합을 담아주기 위해 mMap에 m의 부분 합과 부분 합의 개수을 저장한다.
+        Map<Integer, Integer> mMap = new HashMap<>();
         for(int i = 0; i < m; i++) {
             int sum = 0;
             for(int j = i; j < m; j++) {
                 sum += mArr[j];
 
-                mList.add(sum);
+                mMap.put(sum, mMap.getOrDefault(sum, 0) + 1);
             }
         }
-        // 투 포인터를 위해 정렬
-        Collections.sort(nList);
-        Collections.sort(mList);
-
-        int i = 0;
-        int j = mList.size() - 1;
 
         long result = 0;
-        while(i < nList.size() && j >= 0) {
-            int nNum = nList.get(i);
-            int mNum = mList.get(j);
+        for(Map.Entry<Integer, Integer> entry : nMap.entrySet()) {
+            int nSum = entry.getKey();
+            long nCount = entry.getValue();
 
-            int sum = nNum + mNum;
-            // 두 부분 합이 t와 같은 경우
-            if(sum == t) {
+            long mCount = mMap.getOrDefault(t - nSum, 0);
 
-                long nCount = 0;
-                long mCount = 0;
-                // n의 부분 합에서 nNum과 같은 값의 개수를 체크
-                while(i < nList.size() && nNum == nList.get(i)) {
-                    nCount++;
-                    i++;
-                }
-
-                // m의 부분 합에서 mNum과 같은 값의 개수를 체크
-                while(j >= 0 && mNum == mList.get(j)) {
-                    mCount++;
-                    j--;
-                }
-
-                // n의 부분 합의 개수 * m의 부분 합의 개수
-                result += nCount * mCount;
-            } else if(sum > t) { // 두 부분 합의 합이 T보다 크면 더 작은 값을 찾기 위해 j--
-                j--;
-            } else { // 두 부분 합의 합이 T보다 작으면 더 큰 값을 찾기 위해 i++
-                i++;
-            }
+            result += nCount * mCount;
         }
-
         System.out.println(result);
     }
 }
