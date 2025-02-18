6f7787f6bb3a073e609740909ee329fd370d7aff
beanteacher <99578261+beanteacher@users.noreply.github.com>
2025-02-18 14:45:34 +0900

    [Gold IV] Title: 뱀, Time: 124 ms, Memory: 14816 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.java
+++ 백준/Gold/3190. 뱀/뱀.java
@@ -11,87 +11,99 @@ public class Main {
             this.y = y;
         }
     }
+
+    static class Action {
+        int second;
+        String direction;
+
+        public Action(int second, String direction) {
+            this.second = second;
+            this.direction = direction;
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int n = Integer.parseInt(br.readLine());
         int k = Integer.parseInt(br.readLine());
-        int[][] arr = new int[n+1][n+1];
 
+        int[][] arr = new int[n+1][n+1];
         for(int i = 0; i < k; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
+
             int row = Integer.parseInt(st.nextToken());
             int column = Integer.parseInt(st.nextToken());
 
-            arr[row][column] = 1;
+            arr[column][row] = 1;
         }
 
         int l = Integer.parseInt(br.readLine());
-        Map<Integer, String> map = new HashMap<>();
-        Queue<Integer> queue = new LinkedList<>();
-        
+        Queue<Action> actionQueue = new LinkedList<>();
+
         for(int i = 0; i < l; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int second = Integer.parseInt(st.nextToken());
-            String dir = st.nextToken();
 
-            queue.offer(second);
-            map.put(second, dir);
+            int x = Integer.parseInt(st.nextToken());
+            String c = st.nextToken();
+
+            actionQueue.offer(new Action(x, c));
         }
-
-        int time = 0;
-        int x = 1;
-        int y = 1;
 
         Queue<Snake> snakeQueue = new LinkedList<>();
         snakeQueue.offer(new Snake(1,1));
 
-        String direct = "RIGHT";
-        while(true) {
-            time++;
+        int second = 0;
+        int x = 1;
+        int y = 1;
+        String direction = "RIGHT";
 
-            if(direct.equals("RIGHT")) y++;
-            else if(direct.equals("LEFT")) y--;
-            else if(direct.equals("UP")) x--;
-            else x++;
+        while (true) {
+            second++;
 
+            if(direction.equals("RIGHT")) x++;
+            else if(direction.equals("LEFT")) x--;
+            else if(direction.equals("UP")) y--;
+            else y++;
+
+            // 벽에 부딪힐 경우 종료
             if(x <= 0 || x > n || y <= 0 || y > n) {
                 break;
             }
 
-            for (Snake s : snakeQueue) {
-                if (s.x == x && s.y == y) { // 현재 이동한 머리와 몸이 겹치는 경우
-
-                    System.out.println(time);
+            // 자기 몸에 걸릴 경우 종료
+            for(Snake snake : snakeQueue) {
+                if(snake.x == x && snake.y == y) {
+                    System.out.println(second);
                     return;
                 }
             }
 
-            // 사과를 만나지 않으면 이 전 칸의 뱀은 이동했으므로 뱀의 위치에서 삭제
-            if(arr[x][y] == 1) { // 사과를 만나면 사과 삭제
-                arr[x][y] = 0;
+            if(arr[x][y] == 1) { // 사과를 만나는 경우 뱀의 몸이 길어졌기 때문에 이 전 위치 제거를 하지 않는다. (꼬리 추가)
+                arr[x][y] = 0; // 사과 제거 (중복으로 사과를 먹으면 안되기 때문)
             } else {
-                snakeQueue.poll();
+                snakeQueue.poll(); // 사과가 없다면 이 전의 위치를 제거 (꼬리 제거)
             }
 
+            // 뱀의 머리 위치 기록
             snakeQueue.offer(new Snake(x, y));
 
-            if(!queue.isEmpty() && time == queue.peek()) {
-                String dir = map.get(queue.poll());
-
-                if(dir.equals("L")) {
-                    if(direct.equals("RIGHT")) direct = "UP";
-                    else if(direct.equals("LEFT")) direct = "DOWN";
-                    else if(direct.equals("UP")) direct = "LEFT";
-                    else direct = "RIGHT";
+            if(!actionQueue.isEmpty() && actionQueue.peek().second == second) {
+                Action action = actionQueue.poll();
+                if(action.direction.equals("D")) {
+                    if(direction.equals("RIGHT")) direction = "DOWN";
+                    else if(direction.equals("LEFT")) direction = "UP";
+                    else if(direction.equals("UP")) direction = "RIGHT";
+                    else direction = "LEFT";
                 } else {
-                    if(direct.equals("RIGHT")) direct = "DOWN";
-                    else if(direct.equals("LEFT")) direct = "UP";
-                    else if(direct.equals("UP")) direct = "RIGHT";
-                    else direct = "LEFT";
+                    if(direction.equals("RIGHT")) direction = "UP";
+                    else if(direction.equals("LEFT")) direction = "DOWN";
+                    else if(direction.equals("UP")) direction = "LEFT";
+                    else direction = "RIGHT";
                 }
-            }
 
+            }
         }
-        System.out.println(time);
+        System.out.println(second);
     }
 }
