edf7f7d4eddd039da520d3e1a0ee88232b669dbe
beanteacher <99578261+beanteacher@users.noreply.github.com>
2025-02-19 20:06:52 +0900

    Update 택배 상자 꺼내기.java

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -2,14 +2,19 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int w, int num) {
-        int answer = 0;
+        
+        // 높이 저장
         int height = (n / w) + 1;
+        // height와 weight로 이차원 배열 선언
         int[][] arr = new int[height][w];
+        // 1~N까지 값을 저장할 count
         int count = 1;
+        // num의 위치를 저장할 column과 row
         int column = 0;
         int row = 0;
         
         for(int i = 0; i < height; i++) {
+            // i가 0이거나 짝수면 정방향으로 배열 생성
             if(i % 2 == 0) {
                 for(int j = 0; j < w; j++) {
                     if(count > n) break;
@@ -22,7 +27,7 @@ class Solution {
                     arr[i][j] = count++;
                 }
                 
-            } else {
+            } else { // i가 홀수면 역방향으로 배열 생성
                 for(int j = w-1; j >= 0; j--) {
                     if(count > n) break;
                     
@@ -36,9 +41,11 @@ class Solution {
             }
         }
         
-        for(int i = height - 1; i >= column; i--) {
+        int answer = 0;
+        
+        // 저장된 위치부터 위에 상자가 있으면 상자를 빼야하므로 answer++
+        for(int i = column; i < height; i++) {
             if(arr[i][row] != 0) {
-                System.out.println(arr[i][row]);
                 answer++;
             }
         }
