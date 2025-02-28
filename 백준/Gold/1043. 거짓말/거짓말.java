c95fa09917055842da95e3721fc1c4a0327f7dd8
beanteacher <99578261+beanteacher@users.noreply.github.com>
2025-02-28 10:11:39 +0900

    [Gold IV] Title: 거짓말, Time: 108 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.java
+++ 백준/Gold/1043. 거짓말/거짓말.java
@@ -9,35 +9,37 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        Queue<Integer> queue = new LinkedList<>();
-        boolean[] visited = new boolean[n+1];
+        Queue<Integer> queue = new LinkedList<>(); // bfs를 위한 queue
+        boolean[] visited = new boolean[n]; // 방문 여부를 판단할 visited
+
         st = new StringTokenizer(br.readLine());
         int truthNum = Integer.parseInt(st.nextToken()); // 진실을 알고있는 인원의 수
+        
         for(int i = 0; i < truthNum; i++) {
             int truthPerson = Integer.parseInt(st.nextToken()) - 1;
             queue.offer(truthPerson); // 진실을 알고 있는 사람을 1로 초기화
-            visited[truthPerson] = true;
+            visited[truthPerson] = true; // 진실을 아는 사람은 방문 true
         }
 
-        Map<Integer, List<Integer>> adjList = new HashMap<>();
+        Map<Integer, List<Integer>> adjList = new HashMap<>(); // 인접리스트 구현 
         for(int i = 0; i < n; i++) {
             adjList.put(i, new ArrayList<>());
         }
-        List<int[]> partyList = new ArrayList<>();
+        List<int[]> partyList = new ArrayList<>(); // 파티 정보를 담을 List
         for(int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
 
             int participantsNum = Integer.parseInt(st.nextToken()); // 해당 파티의 참가 인원 수
-            int[] partyArr = new int[participantsNum];
+            int[] partyArr = new int[participantsNum]; // 해당 파티에 참가한 인원을 담을 배열
 
             for(int j = 0; j < participantsNum; j++) {
                 partyArr[j] = Integer.parseInt(st.nextToken()) - 1;
             }
-            partyList.add(partyArr);
+            partyList.add(partyArr); // i번 파티에 참가한 인원들 담기
 
             for(int j = 0; j < participantsNum; j++) {
                 for(int k = j+1; k < participantsNum; k++) {
-                    if(!adjList.get(partyList.get(i)[j]).contains(partyList.get(i)[k])) {
+                    if(!adjList.get(partyList.get(i)[j]).contains(partyList.get(i)[k])) { // i번 파티에 참가한 j와 k 모두에게 무방향 그래프 제공
                         adjList.get(partyList.get(i)[j]).add(partyList.get(i)[k]);
                         adjList.get(partyList.get(i)[k]).add(partyList.get(i)[j]);
                     }
@@ -45,12 +47,13 @@ public class Main {
             }
         }
 
+        // bfs 탐색 시작
         while (!queue.isEmpty()) {
-            int node = queue.poll();
-            for(int i = 0; i < adjList.get(node).size(); i++) {
-                if(!visited[adjList.get(node).get(i)]) {
+            int node = queue.poll(); // queue에서 제거한 인원 -> 진실을 아는 사람
+            for(int i = 0; i < adjList.get(node).size(); i++) { // 진실을 아는 사람과 같이 파티를 진행한 사람들을 모두 true로 변경
+                if(!visited[adjList.get(node).get(i)]) { 
                     visited[adjList.get(node).get(i)] = true;
-                    queue.offer(adjList.get(node).get(i));
+                    queue.offer(adjList.get(node).get(i)); // bfs 진행
                 }
             }
         }
@@ -59,7 +62,7 @@ public class Main {
         for(int[] partyArr : partyList) {
             boolean check = false;
             for(int i = 0; i < partyArr.length; i++) {
-                if(visited[partyArr[i]]) {
+                if(visited[partyArr[i]]) { // 진실을 아는 파티이면 result--를 하기 위해 check를 true로 변경
                     check = true;
                     break;
                 }
