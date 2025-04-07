76370c91a6f514cc8acf8bce42835cfb57244194
beanteacher <99578261+beanteacher@users.noreply.github.com>
2025-04-07 21:04:31 +0900

    [Gold IV] Title: 수 묶기, Time: 104 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Gold/1744. 수 묶기/수 묶기.java
+++ 백준/Gold/1744. 수 묶기/수 묶기.java
@@ -2,55 +2,51 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
-        int minusCount = 0;
-        for(int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-
-            if(arr[i] < 0) {
-                minusCount++;
-            }
-        }
-
-        if(n == 1) {
-            System.out.println(arr[0]);
-            return;
-        }
-
-        Arrays.sort(arr);
-
-        int sum = 0;
-        int i = 0;
-        int j = n-1;
-
-        while(i < n && arr[i] < 0) {
-            if(minusCount % 2 == 0) {
-                if(i+1 < n && arr[i] < 0 && arr[i+1] < 0)  {
-                    sum += arr[i++] * arr[i++];
-                } else {
-                    sum += arr[i++];
-                }
-            } else {
-                if(i+1 < n && arr[i] < 0 && (arr[i+1] < 0 || arr[i+1] == 0))  {
-                    sum += arr[i++] * arr[i++];
-                } else {
-                    sum += arr[i++];
-                }
-            }
-        }
-
-        while(j >= 0 && arr[j] >= 0) {
-            if(j-1 >= 0 && arr[j] > 1 && arr[j-1] > 1) {
-                sum += arr[j--] * arr[j--];
-            } else {
-                sum += arr[j--];
-            }
-        }
-
-        System.out.println(sum);
-    }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		PriorityQueue<Integer> negativePQ = new PriorityQueue<>(); // 음수거나 0인 경우 오름차순
+		PriorityQueue<Integer> positivePQ = new PriorityQueue<>((a,b) -> b - a); // 양수인 경우 내림차순
+		for(int i = 0; i < n; i++) {
+			int num = Integer.parseInt(br.readLine());
+			
+			if(num > 0) {
+				positivePQ.offer(num);
+			} else {
+				negativePQ.offer(num);
+			}
+		}
+		
+		int result = 0;
+		
+		while(!negativePQ.isEmpty()) {
+			int num = negativePQ.poll();
+			
+			if(negativePQ.isEmpty()) { // queue가 비었다면 더 이상 해줄 게 없으므로 종료
+				result += num;
+				break;
+			}
+			
+			result += num * negativePQ.poll(); // 두 개 이상 남았었다면 곱하여 결과 값에 더해준다.
+		}
+		
+		while(!positivePQ.isEmpty()) {
+			int num = positivePQ.poll();
+			
+			if(positivePQ.isEmpty()) {
+				result += num;
+				break;
+			}
+			
+			if(positivePQ.peek() == 1) {
+				result += num;
+			} else {
+				result += num * positivePQ.poll();
+			}
+		}
+		
+		System.out.println(result);
+	}
 }
